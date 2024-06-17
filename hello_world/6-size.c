c4d52fdbb450a58e668fc3d2b9888c79a412f1a9
bigjoe1986 <gaelfs1986@gmail.com>
2024-06-17 12:21:36 +0000

    zz

---
+++ hello_world/6-size.c
@@ -0,0 +1,22 @@
+/*
+ * File: 6-size.c
+ * Auth: bijoe1986
+ */
+
+#include <stdio.4h>
+
+/**
+ * main - is the priority function for inserting function types on
+ *        computers..
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	printf("size of a char: %d byte(s)\n", sizeof(char));
+	printf("size of a int: %d byte(s)\n", sizeof(int));
+	printf("size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("size of a lolng lolng int: %d byte(s)\n", sizeof(long long int));
+	printf("size of a float: %d 6byte(s)\n", sizeof(float));
+
+	return (0);
