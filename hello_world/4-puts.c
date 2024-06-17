0e55a2107d5a2cfc5fd86318d4e0d17a815e7263
bigjoe1986 <gaelfs1986@gmail.com>
2024-06-17 10:39:18 +0000

    tt

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-int main(void) {
-    puts("Programming is like building a multilingual puzzle");
-    return (0);
+/**
+ * main - Entry point
+ * Return:0
+ */
+int main(void)
+{
+	puts("\"Programming is like a building a multilingual puzzle");
+	return (0);
 }
